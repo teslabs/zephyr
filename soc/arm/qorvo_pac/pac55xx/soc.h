be0fb2f1eb04394233dfa2e6bb90ef7ac9b86317
Gerard Marull-Paretas <gerard.marull@nordicsemi.no>
2023-06-29 21:32:43 +0200

    soc: arm: qorvo_pac: pac55xx: add initial support for PAC55XX SoCs
    This patch adds initial support for PAC55XX SoCs.
    
    Signed-off-by: Gerard Marull-Paretas <gerard.marull@nordicsemi.no>

---
+++ soc/arm/qorvo_pac/pac55xx/soc.h
@@ -0,0 +1,11 @@
+/*
+ * Copyright (c) 2023, Teslabs Engineering S.L.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef ZEPHYR_SOC_ARM_QORVO_PAC5XXX_PAC55XX_SOC_H_
+#define ZEPHYR_SOC_ARM_QORVO_PAC5XXX_PAC55XX_SOC_H_
+
+/* this header is currently required by the ARM architecture port */
+
+#endif /* ZEPHYR_SOC_ARM_QORVO_PAC5XXX_PAC55XX_SOC_H_ */
