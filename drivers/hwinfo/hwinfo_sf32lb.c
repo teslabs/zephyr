2ef6286bb3d057706bbeb39cb006e32c84b33e86
Gerard Marull-Paretas <gerard@teslabs.com>
2025-10-15 18:45:47 +0200

    drivers: hwinfo: add driver for SF32LB platform
    Add hwinfo driver for SF32LB platform so we can obtain e.g. device
    unique ID.
    
    Signed-off-by: Gerard Marull-Paretas <gerard@teslabs.com>

---
+++ drivers/hwinfo/hwinfo_sf32lb.c
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) 2025 Core Devices LLC
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <zephyr/drivers/hwinfo.h>
+
+#include <register.h>
+
+ssize_t z_impl_hwinfo_get_device_id(uint8_t *buffer, size_t length)
+{
+	return 0;
+}
